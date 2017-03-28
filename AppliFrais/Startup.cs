bd7665c9eaeb40da96fee7fa7ac0ea4fe38912cc
Yuks <beck.nicolas0@gmail.com>
2017-03-28 19:59:58 +0200

    Ajoutez des fichiers projet.

---
+++ AppliFrais/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppliFrais.Startup))]
+namespace AppliFrais
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
